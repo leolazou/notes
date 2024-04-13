8947addcff0f6350428337a604256e5d8b2847a6
leolazou <ileonidasf@gmail.com>
2024-04-13 11:08:14 +0200

    Space title changed to "leolazou"

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Leo's notes",
+    pageTitle: "leolazou",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
