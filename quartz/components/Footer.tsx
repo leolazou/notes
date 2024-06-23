3a40e62baebd63e682de540d7908b5a5f514ee19
leolazou <ileonidasf@gmail.com>
2024-06-24 01:01:07 +0200

    Slightly more minimalistic footer

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -24,7 +24,7 @@ export default ((opts?: Options) => {
           </ul>
           <p>
             {i18n(cfg.locale).components.footer.createdWith}{" "}
-            <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+            <a href="https://quartz.jzhao.xyz/">Quartz</a>
           </p>
         </div>
       </footer>
